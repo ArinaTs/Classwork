6ff00fc1b73cc36b37c28c83f7a7c25d2b636e78
itmm-2213-a <itmm-2213-a@E.local>
2022-09-29 09:56:47 +0300

    Массив задан, заполнен и выведен на экран

--- ArraysActions_Easy/arrays_easy.c
+++ ArraysActions_Easy/arrays_easy.c
@@ -1,10 +1,22 @@
 #include<stdio.h>
+#include<locale.h>
 int main() {
-	//объ€вление переменных
-	//инициализаци€
-	//выполнение цели
-	//вывод результата
-	//завершение программы
+	setlocale(LC_ALL, "rus");
+	int size = 0;
+	printf("¬ведите размер массива: ");
+	scanf_s("%d", &size);
+	int mass[10000];
+	while (size <= 0 || size > 10000) {
+		printf("¬ведите корректный размер массива: ");
+		scanf_s("%d", &size);
+	}
+	for (int i = 0; i < size; i++) {
+		mass[i] = -10 + rand()%21;
+	}
+	printf("ћассив состоит из чисел: \n");
+	for (int i = 0; i < size; i++) {
+		printf("%d ", mass[i]);
+	}
 	system("pause");
 	return 0;
 }
