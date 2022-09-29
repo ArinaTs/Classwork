b2467114af1adb5806907b1b27fae514a7259324
itmm-2213-a <itmm-2213-a@E.local>
2022-09-29 10:35:26 +0300

    кратные числа возведены в степень, массив выведен в обратном порядке

--- ArraysActions_Easy/arrays_easy.c
+++ ArraysActions_Easy/arrays_easy.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<locale.h>
+#include<math.h>
 int main() {
 	setlocale(LC_ALL, "rus");
 	int size = 0;
@@ -17,6 +18,27 @@ int main() {
 	for (int i = 0; i < size; i++) {
 		printf("%d ", mass[i]);
 	}
+	int number;
+	printf("\n¬ведите число: ");
+	scanf_s("%d", &number);
+	int st;
+	printf("¬ведите степень: ");
+	scanf_s("%d", &st);
+	for (int i = 0; i < size; i++) {
+		if (mass[i] % number == 0) {
+			mass[i] = pow(mass[i], st);
+		}
+	printf("%d ", mass[i]);
+	}
+	int mass1[10000];
+	for (int i = size; i >= 0; i--) {
+		mass1[size - i - 1] = mass[i];
+	}
+	printf("\n");
+	for (int i = 0; i < size; i++) {
+		printf("%d ", mass1[i]);
+	}
+	printf("\n");
 	system("pause");
 	return 0;
 }
